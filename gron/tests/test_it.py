8eafbfc25037cd0097793cc27de705f0d504b595
podhmo <ababjam61+github@gmail.com>
2016-08-21 01:45:17 +0900

    table driven test

--- gron/tests/test_it.py
+++ gron/tests/test_it.py
@@ -20,11 +20,19 @@ class Tests(unittest.TestCase):
             data = json.load(rf)
         return self._getTarget()(data)
 
-    def test_one(self):
-        target_file = "./data/one.json"
-        answer_file = "./data/one-answer.json"
+    def test_it(self):
+        candidates = [
+            ("./data/one.json", "./data/one-answer.json"),
+            ("./data/two.json", "./data/two-answer.json"),
+            ("./data/two-b.json", "./data/two-b-answer.json"),
+            ("./data/three.json", "./data/three-answer.json"),
+        ]
 
-        result = self._callFUT(target_file)
-        expected = self._get_answer(answer_file)
-        result = "\n".join(result)
-        self.assertEqual(result, expected)
+        for target_file, answer_file in candidates:
+            with self.subTest(target_file=target_file, answer_file=answer_file):
+                result = self._callFUT(target_file)
+                expected = self._get_answer(answer_file)
+                result = "\n".join(result)
+
+                msg = "\n".join(["\n@result", result, "=", "@expected", expected])
+                self.assertEqual(result, expected, msg=msg)
